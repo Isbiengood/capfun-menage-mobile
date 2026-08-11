7ebdb02bbedb97475ae8bba0b236066f7cf61542
Isbiengood <lawlessmaison@gmail.com>
2026-08-11 11:17:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,4 @@
-/*
- * ============================================================
- * CAPFUN MÉNAGE MOBILE — CONFIGURATION GITHUB PAGES
- * ============================================================
- *
- * Remplir uniquement les deux valeurs ci-dessous.
- */
-
 window.CAPFUN_CONFIG = {
-  /*
-   * URL /exec de l'application Web Apps Script actuelle.
-   */
-  APPS_SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbwnRLlP30z3n0it-fA_vF5cHa5sJji399v_bYqtYkrel4-cRiKuHvV1DVjDkfa39_uP/exec",
-
-  /*
-   * App ID public OneSignal.
-   */
-  ONESIGNAL_APP_ID:
-    "54be8a5d-df55-452a-a5c9-d3559ecd9297"
+  APPS_SCRIPT_URL: "COLLER_ICI_URL_APPS_SCRIPT_EXEC",
+  ONESIGNAL_APP_ID: "54be8a5d-df55-452a-a5c9-d3559ecd9297"
 };
