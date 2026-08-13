e4696f2a14394a5d240e151a790dffcb60976267
Isbiengood <lawlessmaison@gmail.com>
2026-08-13 13:58:14 +0200

    backup: CampManager V3.2.5 avant optimisation

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,10 @@
+/*
+ * CAMPMANAGER V3.3
+ *
+ * Même service worker qu'avant pour conserver OneSignal.
+ * On ajoute simplement le module de cache hors connexion.
+ *
+ * Ne pas déplacer ce fichier : garder le même URL.
+ */
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+importScripts("./campmanager-offline-worker.js");
